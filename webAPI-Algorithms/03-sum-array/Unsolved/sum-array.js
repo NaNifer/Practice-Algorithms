2025e9a5c40362058439dd3b295d9871298a9575
Nifer Kilakila <nifercritter@gmail.com>
2022-11-09 12:58:00 -0800

    sum array

--- webAPI-Algorithms/03-sum-array/Unsolved/sum-array.js
+++ webAPI-Algorithms/03-sum-array/Unsolved/sum-array.js
@@ -1,3 +1,9 @@
 // Write code to add all the numbers in `arr` and return the total
 
-var sumArray = function(arr) {};
+var sumArray = function (arr) {
+    let total = 0;
+    for (const i in arr) {
+        total += arr[i];
+    }
+    return total
+};
