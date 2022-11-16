2f222df94cb53a3598cc02de58f26c734a4e6d59
Nifer Kilakila <nifercritter@gmail.com>
2022-11-15 20:45:48 -0800

    vowel count

--- 3rdParty-Algorithms/03-vowel-count/Unsolved/vowel-count.js
+++ 3rdParty-Algorithms/03-vowel-count/Unsolved/vowel-count.js
@@ -1,9 +1,10 @@
 // Write code to return the the number of vowels in `str`
 
 var vowelCount = function (str) {
-    const regex = /[aeiou]/g;
+    const regex = /[aeiou]/gi;
+    if (str.length === 0)
+        return 0;
     const found = str.match(regex);
     return found.length;
-
 };
 
