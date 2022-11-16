3f34896661660cb8620edf0b3d555f69e3af45af
Nifer Kilakila <nifercritter@gmail.com>
2022-11-15 22:38:38 -0800

    solution 1 for palindrone

---
+++ serversideAPI-Algorithms/01-is-palindrome/Unsolved/is-palindrome.js
@@ -0,0 +1,15 @@
+// Write code that returns true if `str` is a palindrome, and false if `str` is not a palindrome
+
+var isPalindrome = function (str) {
+    let length = str.length-1
+    let result = "";
+
+    for (let i = 0; i < str.length; i++) {
+        let letter = str.slice(length, length+1);
+        console.log(letter, i, length);
+        length--;
+        result = result.concat(letter);
+    }
+    return (result === str);
+};
+
