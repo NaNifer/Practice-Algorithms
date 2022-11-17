86d867edddc1e26e1f1c52faac725ae9f92e3e33
Nifer Kilakila <nifercritter@gmail.com>
2022-11-17 14:52:48 -0800

    Working on factorial

--- serversideAPI-Algorithms/02-factorial/Unsolved/factorial.js
+++ serversideAPI-Algorithms/02-factorial/Unsolved/factorial.js
@@ -1,3 +1,10 @@
 // Write code to create a function that returns the factorial of `num`
+let num = 10;
+var factorial = function(num) {
+    arr = [];
+    for (i=0; i< num; num--) {
+        arr.unshift(num);
+    };
+    return arr;
+}
 
-var factorial = function(num) {};
