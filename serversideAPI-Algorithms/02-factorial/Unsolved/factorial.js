fc8e47be9bf7771e5e6d1e93af4d4f7de2ff6fdc
Nifer Kilakila <nifercritter@gmail.com>
2022-11-19 23:43:46 -0800

    factorial mess

--- serversideAPI-Algorithms/02-factorial/Unsolved/factorial.js
+++ serversideAPI-Algorithms/02-factorial/Unsolved/factorial.js
@@ -1,5 +1,4 @@
 // Write code to create a function that returns the factorial of `num`
-let num = 10;
 var factorial = function(num) {
     arr = [];
     for (i=0; i< num; num--) {
@@ -7,4 +6,4 @@ var factorial = function(num) {
     };
     return arr;
 }
-
+factorial(10);
