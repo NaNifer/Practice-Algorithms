4c99b0ecf70fe2934770200da80e89618c1551b5
Nifer Kilakila <nifercritter@gmail.com>
2022-11-25 15:03:18 -0800

    title case

--- serversideAPI-Algorithms/03-title-case/Unsolved/title-case.js
+++ serversideAPI-Algorithms/03-title-case/Unsolved/title-case.js
@@ -1,3 +1,7 @@
 // Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
-var titleCase = function(str) {};
+var titleCase = function(str) {
+    return str.replace(/\w\S*/g, function(txt){
+        return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+};
