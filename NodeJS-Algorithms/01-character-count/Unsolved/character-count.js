86050627d68407e0b45642aee0d165809d270f12
Nifer Kilakila <nifercritter@gmail.com>
2022-11-26 20:42:06 -0800

    started character count

--- NodeJS-Algorithms/01-character-count/Unsolved/character-count.js
+++ NodeJS-Algorithms/01-character-count/Unsolved/character-count.js
@@ -1,3 +1,10 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
-var characterCount = function(str) {};
+var characterCount = function(str) {
+    let length = str.length+1;
+
+    for (let i = 0; i < length; i++);
+
+
+
+};
