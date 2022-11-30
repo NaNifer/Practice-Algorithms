20102e28ddb827c86f13e648e9885c7976bd3e25
Nifer Kilakila <nifercritter@gmail.com>
2022-11-29 21:36:07 -0800

    Working on character count

--- NodeJS-Algorithms/02-product-of-largest-two/Unsolved/product-of-largest-two.js
+++ NodeJS-Algorithms/02-product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -1,6 +1,9 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
 var productOfLargestTwo = function(arr) {
-
+    function compareNumbers(a, b) {
+        return a - b;
+      }
+    Array.sort(compareNumbers)
     for (let i = 0; i < length; i++);
 };
