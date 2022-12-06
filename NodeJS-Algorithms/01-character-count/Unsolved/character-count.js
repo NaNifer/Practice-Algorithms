41952d268cee6fc8a8db519b0d6d09dac97e74c6
Nifer Kilakila <nifercritter@gmail.com>
2022-12-05 22:27:27 -0800

    Charater count, solution 1/2 working

--- NodeJS-Algorithms/01-character-count/Unsolved/character-count.js
+++ NodeJS-Algorithms/01-character-count/Unsolved/character-count.js
@@ -1,12 +1,20 @@
 // Write code to create a function that accepts a string and returns an object containing the number of times each character appears in the string
 
-var characterCount = function(str) {
-    let length = str.length+1;
+let str = "hello"
 
-    for (let i = 0; i < length; i++) {
-        
+var characterCount = function (str) {
+    let charMap = new Map();
+    const count = 0;
+    for (const key of str) {
+        charMap.set(key, count);
     }
-
-
+    for (const key of str) {
+        let count = charMap.get(key);
+        charMap.set(key, count + 1);
+      }
+      for (const [key,value] of charMap) {
+        console.log(key,value);
+      }
 
 };
+
