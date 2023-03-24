a997e49eb3ba08a674049b6801146cac557654c6
Nifer Kilakila <nifercritter@gmail.com>
2023-03-23 20:24:34 -0700

    productOfLargestTwo in nodeJS

--- NodeJS-Algorithms/02-product-of-largest-two/Unsolved/product-of-largest-two.js
+++ NodeJS-Algorithms/02-product-of-largest-two/Unsolved/product-of-largest-two.js
@@ -1,9 +1,10 @@
 // Write code to create a function that accepts an array of numbers, finds the largest two numbers, and returns the product of the two
 
-var productOfLargestTwo = function(arr) {
-    function compareNumbers(a, b) {
-        return a - b;
-      }
-    Array.sort(compareNumbers)
-    for (let i = 0; i < length; i++);
-};
+const productOfLargestTwo = (arr) => {
+  const sortedArr = arr.sort(compareNumbers);
+  function compareNumbers(a, b) {
+    return a - b;
+  }
+  return sortedArr[arr.length - 1] * sortedArr[arr.length - 2];
+}
+
