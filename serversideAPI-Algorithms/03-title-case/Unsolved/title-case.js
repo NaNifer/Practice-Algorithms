6a2a8e0a1e5154e285811faebed332902ebfb8fd
Nifer Kilakila <nifercritter@gmail.com>
2023-03-25 19:27:05 -0700

    refactor titleCase in serverside api

--- serversideAPI-Algorithms/03-title-case/Unsolved/title-case.js
+++ serversideAPI-Algorithms/03-title-case/Unsolved/title-case.js
@@ -1,4 +1,3 @@
-// Write code to create a function takes a string and returns a new string with the first letters of each word capitalized
 
 var titleCase = function(str) {
     return str.replace(/\w\S*/g, function(txt){
