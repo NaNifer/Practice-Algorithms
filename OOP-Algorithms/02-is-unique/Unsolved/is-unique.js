9a67343df1f257e645e63bd459407c28a044e7e4
Nifer Kilakila <nifercritter@gmail.com>
2023-03-31 15:47:16 -0700

    Working on isUnique in OOP

--- OOP-Algorithms/02-is-unique/Unsolved/is-unique.js
+++ OOP-Algorithms/02-is-unique/Unsolved/is-unique.js
@@ -1,4 +1,18 @@
 // Write code to create a function that accepts an array numbers
 // Return `true` is no number appears in the array more than once, else return `false`
 
-var isUnique = function(arr) {};
+var isUnique = function(arr) {
+    const arrMap = {};
+
+    for (var i = 0; i <= arr.length; i++) {
+        const currentNum = arr[i];
+
+        return arr.includes(currentNum);
+
+        if (arrMap[currentNum] === true) {
+            return false
+        }
+    }
+    return true
+};
+
