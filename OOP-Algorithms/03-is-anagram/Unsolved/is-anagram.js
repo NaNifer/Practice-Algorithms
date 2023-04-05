f3ceb0a31e7dc2b2f7a81b4f434bcd2ddd7d0049
Nifer Kilakila <nifercritter@gmail.com>
2023-04-04 20:51:16 -0400

    Anagram in OOP

--- OOP-Algorithms/03-is-anagram/Unsolved/is-anagram.js
+++ OOP-Algorithms/03-is-anagram/Unsolved/is-anagram.js
@@ -1,4 +1,28 @@
 // Write code to create a function that accepts two strings
 // Return true if the strings are anagrams of each other, otherwise return false
 
-var isAnagram = function(strA, strB) {};
+const isAnagram = function(strA, strB) {
+    if (strA.length !== strB.length) {
+      return false;
+    }
+  
+    const aLetterMap = {};
+    const bLetterMap = {};
+    const strLength = strA.length;
+  
+    for (const i = 0; i < strLength; i++) {
+      const aLetter = strA[i];
+      const bLetter = strB[i];
+  
+      aLetterMap[aLetter] = (aLetterMap[aLetter] || 0) + 1;
+      bLetterMap[bLetter] = (bLetterMap[bLetter] || 0) + 1;
+    }
+  
+    for (const key in aLetterMap) {
+      if (aLetterMap[key] !== bLetterMap[key]) {
+        return false;
+      }
+    }
+  
+    return true;
+  };
