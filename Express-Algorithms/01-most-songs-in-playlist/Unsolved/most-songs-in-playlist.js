537a12f32c6b490719fe8e9f57b33e1ffe16200c
Nifer Kilakila <nifercritter@gmail.com>
2023-04-07 13:04:55 -0400

    Most songs in Express

--- Express-Algorithms/01-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
+++ Express-Algorithms/01-most-songs-in-playlist/Unsolved/most-songs-in-playlist.js
@@ -2,6 +2,21 @@
 // Each number represents the duration of a song in minutes
 // Return the maximum number of songs from the list that can fit in a 60 minute playlist
 
-var mostSongsInPlaylist = function(arr) {
-  // add your code here...
+const mostSongsInPlaylist = function(arr) {
+  const sortedArr = arr.sort();
+  const totalMins = 0;
+  const totalSongs = 0;
+
+  for (const i = 0; i < sortedArr.length; i++) {
+    const currentSongDuration = sortedArr[i];
+
+    if (totalMins + currentSongDuration <= 60) {
+      totalMins += currentSongDuration;
+      totalSongs++;
+    } else {
+      return totalSongs;
+    }
+  }
+
+  return totalSongs;
 };
