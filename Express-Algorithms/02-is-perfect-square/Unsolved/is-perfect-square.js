8c65fb7e7349fb05fab129813021b4237854ffa2
Nifer Kilakila <nifercritter@gmail.com>
2023-04-10 21:28:27 -0700

    Express is perfect square

--- Express-Algorithms/02-is-perfect-square/Unsolved/is-perfect-square.js
+++ Express-Algorithms/02-is-perfect-square/Unsolved/is-perfect-square.js
@@ -1,4 +1,18 @@
 // Write code to create a function that accepts a number and returns true if the number is a perfect square, otherwise it returns false.
 // You may not use the built-in Math.sqrt method
 
-var isPerfectSquare = function(num) {};
+const isPerfectSquare = function(num) {
+    const i = 0;
+  
+    while (true) {
+      const currentSquare = i * i;
+  
+      if (currentSquare === num) {
+        return true;
+      } else if (currentSquare > num) {
+        return false;
+      } else {
+        i++;
+      }
+    }
+  };
