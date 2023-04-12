6390a0e368585869e200758e626a2100260b245b
Nifer Kilakila <nifercritter@gmail.com>
2023-04-11 21:06:30 -0700

    Array search in Express

--- Express-Algorithms/03-array-search-2d/Unsolved/array-search-2d.js
+++ Express-Algorithms/03-array-search-2d/Unsolved/array-search-2d.js
@@ -2,4 +2,15 @@
 // Each 2D element contains either the string "X" or "O" (both capitalized)
 // Return the number of times "X" appears in the 2D array
 
-var arraySearch2D = function(arr) {};
+const arraySearch2D = function(arr) {
+    const count = 0;
+  
+    for (const i = 0; i < arr.length; i++) {
+      for (const j = 0; j < arr[i].length; j++) {
+        if (arr[i][j] === "X") {
+          count++;
+        }
+      }
+    }
+    return count;
+  };
