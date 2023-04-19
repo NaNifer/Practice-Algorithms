e5531c4c9ddf4d9cb4d569f5b174a3015b3753d6
Nifer Kilakila <nifercritter@gmail.com>
2023-04-19 16:30:45 -0700

    Merge sort in SQL algorithms

--- SQL-Algorithms/03-merge-sorted/Unsolved/merge-sorted.js
+++ SQL-Algorithms/03-merge-sorted/Unsolved/merge-sorted.js
@@ -1,3 +1,35 @@
 // Write code to merge two sorted arrays into a new sorted array
 
-var mergeSorted = function (arr1, arr2) {};
+const mergeSorted = function (arr1, arr2) {
+  let result = [];
+
+  let idx1 = 0;
+  let idx2 = 0;
+
+  while (idx1 < arr1.length && idx2 < arr2.length) {
+    let num1 = arr1[idx1];
+    let num2 = arr2[idx2];
+
+    if (num1 < num2) {
+      result.push(num1);
+      idx1++;
+    } else {
+      result.push(num2);
+      idx2++;
+    }
+  }
+
+  while (idx1 < arr1.length) {
+    let num1 = arr1[idx1];
+    result.push(num1);
+    idx1++;
+  }
+
+  while (idx2 < arr2.length) {
+    let num2 = arr2[idx2];
+    result.push(num2);
+    idx2++;
+  }
+
+  return result;
+};
