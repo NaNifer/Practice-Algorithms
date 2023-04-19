c65857dfd57ec0b75c698183ee1d1c3be273de6a
Nifer Kilakila <nifercritter@gmail.com>
2023-04-18 21:17:47 -0700

    zeros and ones in SQL algorythms

--- SQL-Algorithms/02-zeroes-and-ones/Unsolved/zeroes-and-ones.js
+++ SQL-Algorithms/02-zeroes-and-ones/Unsolved/zeroes-and-ones.js
@@ -2,4 +2,19 @@
 // Return true if there are an equal number of 0s and 1s
 // Else return false
 
-var zeroesAndOnes = function(str) {};
+const zeroesAndOnes = function(str) {
+    const zeroes = 0;
+    const ones = 0;
+  
+    for (const i = 0; i < str.length; i++) {
+      const num = str[i];
+  
+      if (num === "0") {
+        zeroes++;
+      } else if (num === "1") {
+        ones++;
+      }
+    }
+  
+    return zeroes === ones;
+  };
