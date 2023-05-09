e405c6a3795c7d1dc9b23b4b9c8aee755e269b9f
Nifer Kilakila <nifercritter@gmail.com>
2023-05-08 21:23:44 -0700

    Smallest diff in algorithms

--- PWA-Algorithms/03-smallest-difference/Unsolved/smallest-difference.js
+++ PWA-Algorithms/03-smallest-difference/Unsolved/smallest-difference.js
@@ -3,4 +3,32 @@
 
 // TODO: Solve the function below:
 
-var smallestDifference = function (arr1, arr2) {};
+const smallestDifference = function(arr1, arr2) {
+    let left = 0;
+    let right = 0;
+    let bestPair = [];
+    let bestDiff = Infinity;
+  
+    while (left < arr1.length && right < arr2.length) {
+      let leftVal = arr1[left];
+      let rightVal = arr2[right];
+      let currentDiff = Math.abs(rightVal - leftVal);
+  
+      if (currentDiff < bestDiff) {
+        bestDiff = currentDiff;
+        bestPair = [leftVal, rightVal];
+  
+        if (bestDiff === 0) {
+          return bestPair;
+        }
+      }
+  
+      if (leftVal < rightVal) {
+        left++;
+      } else if (rightVal < leftVal) {
+        right++;
+      }
+    }
+  
+    return bestPair;
+  };
