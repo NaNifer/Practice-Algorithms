6d8c1ccc82d0e27cf05ee84a5b2a6f466a55fa83
Nifer Kilakila <nifercritter@gmail.com>
2023-05-11 19:23:28 -0700

    Began max sub array in react

--- React-Algorithms/01-maximum-continuous-subarray/Unsolved/maximum-continuous-subarray.js
+++ React-Algorithms/01-maximum-continuous-subarray/Unsolved/maximum-continuous-subarray.js
@@ -1,3 +1,14 @@
 const maxSumArray = function(numbers) {
-  // Write your solution here
+
+  // One integer, return sum
+
+  if (numbers.length === 1) {
+    return numbers[0];
+    
+    // Two integers or positive integer, return sum
+  }
+  if (numbers.length === 2 || numbers.every((x) => x >= 0)) {
+    return numbers.reduce((a, b) => a + b, 0);
+  }
+  
 };
