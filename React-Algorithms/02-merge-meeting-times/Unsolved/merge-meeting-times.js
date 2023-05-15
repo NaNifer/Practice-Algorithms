c5c039d892bdbd4b93e95475bee0bf1a06bd615e
Nifer Kilakila <nifercritter@gmail.com>
2023-05-15 13:58:55 -0700

    Started merge meeting times in react

--- React-Algorithms/02-merge-meeting-times/Unsolved/merge-meeting-times.js
+++ React-Algorithms/02-merge-meeting-times/Unsolved/merge-meeting-times.js
@@ -1,3 +1,16 @@
 // Write a function that takes in a 2D array of meeting times, where each sub array contains 2 integers representing a meeting start and end time. Return a new 2D array such that overlapping meeting blocks are condensed into combined meeting blocks
 
-var mergeMeetingTimes = function(arr) {};
+const mergeMeetingTimes = function(arr) {
+    let arrCopy = JSON.parse(JSON.stringify(arr));
+    let sortedArr = arrCopy.sort((a, b) => a[0] - b[0]);
+  
+    let result = [sortedArr[0]];
+  
+    for (let i = 1; i < sortedArr.length; i++) {
+      
+
+        
+    }
+  
+    return result;
+  };
