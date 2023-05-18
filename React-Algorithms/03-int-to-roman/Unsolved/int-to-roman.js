53ca11dcd0baae94014f1c9ab4d687699b6e7fec
Nifer Kilakila <nifercritter@gmail.com>
2023-05-17 21:51:22 -0700

    Began to-roman algorithms in react

--- React-Algorithms/03-int-to-roman/Unsolved/int-to-roman.js
+++ React-Algorithms/03-int-to-roman/Unsolved/int-to-roman.js
@@ -1,3 +1,17 @@
 // Write a function that takes in an integer and returns it as a Roman numeral string
 
-var intToRoman = function(num) {};
+const intToRoman = function(num) {
+    let result = "";
+    
+    while (num > 0) {
+      if (num >= 1000) {
+        result += "M";
+        num -= 1000;
+      } else if (num >= 900) {
+        result += "CM";
+        num -= 900;
+      }
+    }
+  
+    return result;
+  };
