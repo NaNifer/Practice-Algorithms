39a877693cac7e085d8c8092381f2a5ee0948012
Nifer Kilakila <nifercritter@gmail.com>
2023-05-20 14:40:43 -0700

    Plus one in Mern algorithms

--- MERN-Algorithms/01-plus-one/Unsolved/plus-one.js
+++ MERN-Algorithms/01-plus-one/Unsolved/plus-one.js
@@ -2,5 +2,27 @@
 // Return the new array
 // Solve without joining the digits array and modify the original array in place
 
-var plusOne = function(digits) {
-};
+const plusOne = function(digits) {
+    let carry = 1;
+  
+    for (let i = digits.length - 1; i >= 0; i--) {
+      let plusOne = digits[i] + carry;
+      let sum;
+      carry = 0;
+  
+      if (plusOne > 9) {
+        carry = 1;
+        sum = 10 - plusOne;
+      } else {
+        sum = plusOne;
+      }
+  
+      digits[i] = sum;
+    }
+  
+    if (carry) {
+      digits.unshift(carry);
+    }
+  
+    return digits;
+  };
