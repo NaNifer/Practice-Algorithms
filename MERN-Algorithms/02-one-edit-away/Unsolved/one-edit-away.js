de9928733b65d55acf866176f7ea1d520f8b2ff2
Nifer Kilakila <nifercritter@gmail.com>
2023-05-22 22:51:29 -0700

    One edit away in Mern algorithms, started

--- MERN-Algorithms/02-one-edit-away/Unsolved/one-edit-away.js
+++ MERN-Algorithms/02-one-edit-away/Unsolved/one-edit-away.js
@@ -1,3 +1,8 @@
-const oneEditAway = function(str1, str2) {
-// TODO: Write function that takes in two strings and returns true if one character away, otherwise false
+const oneEditAway = function (str1, str2) {
+    // TODO: Write function that takes in two strings and returns true if one character away, otherwise false
+
+    // If difference in string lengths is greater than 1 OR strings exactly equal, return false
+    if (str1 === str2 || Math.abs(str1.length - str2.length) > 1) {
+        return false;
+    };
 }
