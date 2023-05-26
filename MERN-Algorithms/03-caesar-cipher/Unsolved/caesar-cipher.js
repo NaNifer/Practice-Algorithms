e059978269fac8a8a1d4f8b537a16801ceee4995
Nifer Kilakila <nifercritter@gmail.com>
2023-05-26 16:18:07 -0700

    Began cipher in MERN

--- MERN-Algorithms/03-caesar-cipher/Unsolved/caesar-cipher.js
+++ MERN-Algorithms/03-caesar-cipher/Unsolved/caesar-cipher.js
@@ -1,3 +1,11 @@
 // Write a function that takes in a string and and an offset and returns a new string with each character moved by the offset
 
-var caesarCipher = function(str, offset) {};
+const caesarCipher = function(str, offset) {
+    let result = "";
+  
+    while (offset < 0) {
+      offset = 26 + offset;
+    }
+  
+  
+  };
