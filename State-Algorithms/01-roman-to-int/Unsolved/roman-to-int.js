41c338ebc147f151d684329fec0862810f92d77f
Nifer Kilakila <nifercritter@gmail.com>
2023-06-02 12:51:35 -0700

    Roman in state algorithms started

--- State-Algorithms/01-roman-to-int/Unsolved/roman-to-int.js
+++ State-Algorithms/01-roman-to-int/Unsolved/roman-to-int.js
@@ -1,3 +1,24 @@
 // Write a function that takes in a Roman Numeral string and returns its integer form
 
-var romanToInt = function(str) {};
+const romanToInt = function (str) {
+
+
+    let numerals = {
+        I: 1,
+        V: 5,
+        X: 10,
+        L: 50,
+        C: 100,
+        D: 500,
+        M: 1000
+    };
+
+
+    for (let i = 0; i < str.length; i++) {
+        let currentNumeral = str[i];
+        let nextNumeral = str[i + 1];
+
+    }
+
+
+};
