1c47a77c5d7134e0e8ffebb9f2c15dc448686928
Nifer Kilakila <nifercritter@gmail.com>
2023-06-09 15:21:09 -0700

    Rotation point in state algorithms

--- State-Algorithms/02-rotation-point/Unsolved/rotation-point.js
+++ State-Algorithms/02-rotation-point/Unsolved/rotation-point.js
@@ -1,3 +1,14 @@
 // Write a function that takes in an array of sorted strings and returns the index of its rotation point if it has one, else return -1
 
-var rotationPoint = function(words) {};
+const rotationPoint = function (words) {
+    let left = 0;
+    let right = words.length;
+
+    while (left <= right) {
+        let center = Math.floor((right + left) / 2);
+
+        if (words[center] < words[center - 1]) {
+            return center;
+        }
+    }
+};
