f9c34bd75a0312a33915cfbca6114a6d694b4ad1
Nifer Kilakila <nifercritter@gmail.com>
2023-06-12 14:11:54 -0700

    Finished rotation point in state algorithms

--- State-Algorithms/02-rotation-point/Unsolved/rotation-point.js
+++ State-Algorithms/02-rotation-point/Unsolved/rotation-point.js
@@ -2,7 +2,7 @@
 
 const rotationPoint = function (words) {
     let left = 0;
-    let right = words.length;
+    let right = words.length - 1;
 
     while (left <= right) {
         let center = Math.floor((right + left) / 2);
@@ -10,5 +10,12 @@ const rotationPoint = function (words) {
         if (words[center] < words[center - 1]) {
             return center;
         }
+        if (words[left] > words[center]) {
+            right = center - 1;
+        } else {
+            left = center + 1;
+        }
     }
+
+    return -1;
 };
