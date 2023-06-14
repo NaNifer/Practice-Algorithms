249159835dc0dde1e7f0ff004c3e08265a2dc22f
Nifer Kilakila <nifercritter@gmail.com>
2023-06-14 16:18:47 -0700

    Finished simplify path

--- State-Algorithms/03-simplify-path/Unsolved/simplify-path.js
+++ State-Algorithms/03-simplify-path/Unsolved/simplify-path.js
@@ -1,15 +1,25 @@
 // Write a function that takes in an absolute file path and simplifies it
 
-const simplifyPath = function(path) {
-    let pathArray = path.split("/");
-  
-    for (let i = 0; i < pathArray.length; i++) {
-      let segment = pathArray[i];
-  
-      if (!segment) {
-        return;
-      }
+const simplifyPath = function (path) {
+  let pathArr = path.split("/");
+  let finalPath = [];
 
+  for (let i = 0; i < pathArr.length; i++) {
+    let segment = pathArr[i];
 
+    if (!segment) {
+      continue;
     }
-  };
+    if (segment === ".") {
+      continue;
+    }
+    if (segment === "..") {
+      finalPath.pop();
+      continue;
+    }
+
+    finalPath.push(segment);
+  }
+
+  return "/" + finalPath.join("/");
+};
